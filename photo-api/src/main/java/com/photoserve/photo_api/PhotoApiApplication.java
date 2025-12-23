152991a372dedc3424dd65ae43d9764a14194e37
Esteban Arrieta <esarrieta11@gmail.com>
2025-12-22 22:16:32 -0800

    added maven project for RestAPI

---
+++ photo-api/src/main/java/com/photoserve/photo_api/PhotoApiApplication.java
@@ -0,0 +1,13 @@
+package com.photoserve.photo_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PhotoApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PhotoApiApplication.class, args);
+	}
+
+}
